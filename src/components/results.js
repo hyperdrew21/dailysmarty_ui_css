c753e291a85efe0fd325429dd70c937aa1c48936
hyperdrew21 <angryfiredragon@gmail.com>
2021-06-26 06:30:29 -0400

    enabled recent and results state to be separate on the home and results page

--- src/components/results.js
+++ src/components/results.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 
 import SearchBar from "./search-bar";
 import Logo from "./logo";
+import ResultsPosts from "./results-posts";
 
 import { connect } from "react-redux";
 import * as actions from "../actions"; 
@@ -17,6 +18,7 @@ class Results extends Component {
             <div>
                 <Logo size={55}/>
                 <SearchBar onSubmit={(query) => this.handleSearchBarSubmit(query)}/>
+                <ResultsPosts/>
             </div>
         );
     }
--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -1,9 +1,10 @@
 import { 
-    SET_RECENT_POSTS
+    SET_RECENT_POSTS,
+    SET_RESULTS_POSTS
 } from "../actions/types";
 
 const INIT_STATE = {
-    posts: [],
+    resultsPosts: [],
     recentPosts: []
 }
 
@@ -12,6 +13,12 @@ export default function(state = INIT_STATE, action) {
         case SET_RECENT_POSTS:
             const recentPosts = action.payload;
         return {...state, recentPosts}
+
+        case SET_RESULTS_POSTS:
+            const resultsPost = action.payload;
+            return {...state, 
+                     resultsPost
+            }
     default:
         return state;
     }
