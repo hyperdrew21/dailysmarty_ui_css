1ac9fbdc517d1d2ecd755dea3ca7ddc9a23eee44
hyperdrew21 <angryfiredragon@gmail.com>
2021-06-25 21:48:00 -0400

    rendered topics to recent posts

--- src/components/post.js
+++ src/components/post.js
@@ -1,10 +1,23 @@
 import React, { Component } from "react";
 
 class Post extends Component {
+
+    renderTopics() {
+        let topics = this.props.associated_topics.map((topic, index) => {
+            return <span className="post-topic" key={index}>{topic}</span>
+        });
+        return topics;
+    }
     render() {
         return (
-            <li>
-                {this.props.title}
+            <li className="recent-posts">
+                <div className="recent-posts-title">
+                    {this.props.title}
+                </div>
+                <div className="recent-posts-topics">
+                    {this.renderTopics()}
+                </div>
+                
             </li>
             );
     }
