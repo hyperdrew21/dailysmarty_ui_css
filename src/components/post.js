bdb30c9a5b87120e17c6de4dd76b67365e34b14d
hyperdrew21 <angryfiredragon@gmail.com>
2021-06-26 19:33:52 -0400

    modified post.js to render helpful links with title and descrip

--- src/components/post.js
+++ src/components/post.js
@@ -8,18 +8,50 @@ class Post extends Component {
         });
         return topics;
     }
-    render() {
-        return (
-            <li className="recent-posts">
-                <div className="recent-posts-title">
-                    {this.props.title}
+
+    renderLinks() {
+        let links = this.props.post_links.map((post_link, index) => {
+            return(
+            <div className="post-link" key={index}>
+                <div className="post-link-box"></div>
+                <div className="post-link-link">
+                    <a href={post_link.link_url}>Useful Link #{index + 1}</a>
                 </div>
-                <div className="recent-posts-topics">
-                    {this.renderTopics()}
-                </div>
-                
-            </li>
+            </div>
             );
+        });
+        return links;
+    }
+
+    render() {
+        if (this.props.type === "recent") {
+            return(
+                    <li className="recent-posts">
+                        <div className="recent-posts-title">
+                            {this.props.title}
+                        </div>
+                        <div className="recent-posts-topics">
+                            {this.renderTopics()}
+                        </div>    
+                    </li>
+                     );
+
+        }else if(this.props.type === "result") {
+            return(
+                <li className="result-post">
+                    <div className="result-post-topics">
+                            {this.renderTopics()}
+                    </div>
+                    <div className="result-post-title">
+                            {this.props.title}
+                    </div>
+                    <div className="result-posts-links">
+                        {this.renderLinks()}
+                    </div>
+                </li>
+                );
+        }
+        
     }
 }
 export default Post;
