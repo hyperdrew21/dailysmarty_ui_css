fe02f5fd9e036601774c3654ad8cb1d8f0a07aa7
hyperdrew21 <angryfiredragon@gmail.com>
2021-06-28 22:21:21 -0400

    added no content to links and fixed post links

--- src/components/post.js
+++ src/components/post.js
@@ -50,6 +50,10 @@ class Post extends Component {
             </div>
             );
         });
+        if(links == 0) {
+            return <div className="no-content">No Post LInks</div>
+        }
+
         return links;
     }
 
